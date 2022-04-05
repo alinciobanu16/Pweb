52d8ee568a1b742401cd796a1cf9d54db0cc5549
alinciobanu <alinciobanu1604@gmail.com>
2022-04-05 15:01:49 +0300

    update

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <div>
+            <h1 className="text-red-500 text-3xl">First Page</h1>
+        </div>
+    );
+};
+
+export default Main;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // content: ['./src/**/*.{html,js,jsx,ts,tsx}', './public/**/*.html'],
+    purge: [],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
