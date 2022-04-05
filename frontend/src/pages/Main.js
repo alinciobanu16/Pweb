0d61d62045c03d0cd296d0e363a93bd3dfe83786
alinciobanu16 <alinciobanu1604@gmail.com>
2022-04-05 21:52:58 +0300

    design first page

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,27 @@
+import "../styles/main.css";
+import { Link } from "react-router-dom";
+
 const Main = () => {
     return (
-        <div>
-            <h1 className="text-red-500 text-3xl">First Page</h1>
+        <div className="main__container">
+            <div className="main__helpers">
+                {/*<h1>Alături de poporul ucrainean</h1>*/}
+                <h1>Together with the ukrainian people</h1>
+
+                <div className="mt-5 flex flex-row">
+                    <Link to="/register" className="main__btn">
+                        Register
+                    </Link>
+                    <Link to="/register" className="main__btn">
+                        Log In
+                    </Link>
+                </div>
+            </div>
+
+            {/*<div className="main__helpers">*/}
+            {/*    <h1>Vrei sa saska0efw poporul ucrainean ?</h1>*/}
+            {/*    <h1>Second</h1>*/}
+            {/*</div>*/}
         </div>
     );
 };
