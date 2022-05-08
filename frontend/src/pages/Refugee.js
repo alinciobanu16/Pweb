15427102943f5007aacbad5bd4a612806e7b277f
Alin Ciobanu <alinciobanu1604@gmail.com>
2022-05-08 23:32:10 +0300

    Get helpers in refugee page

--- frontend/src/pages/Refugee.js
+++ frontend/src/pages/Refugee.js
@@ -1,60 +1,60 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import "../styles/main.css";
 import "../styles/refugee.css";
-import { useEffect, useRef, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useRef, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const sidebarNavItems = [
     {
-        display: 'Dashboard',
-        icon: <i className='bx bx-home'></i>,
-        to: '/',
-        section: ''
+        display: "Dashboard",
+        icon: <i className="bx bx-home"></i>,
+        to: "/",
+        section: "",
     },
     {
-        display: 'Getting Started',
-        icon: <i className='bx bx-star'></i>,
-        to: '/started',
-        section: 'started'
+        display: "Getting Started",
+        icon: <i className="bx bx-star"></i>,
+        to: "/started",
+        section: "started",
     },
     {
-        display: 'Calendar',
-        icon: <i className='bx bx-calendar'></i>,
-        to: '/calendar',
-        section: 'calendar'
+        display: "Calendar",
+        icon: <i className="bx bx-calendar"></i>,
+        to: "/calendar",
+        section: "calendar",
     },
     {
-        display: 'User',
-        icon: <i className='bx bx-user'></i>,
-        to: '/user',
-        section: 'user'
+        display: "User",
+        icon: <i className="bx bx-user"></i>,
+        to: "/user",
+        section: "user",
     },
     {
-        display: 'Orders',
-        icon: <i className='bx bx-receipt'></i>,
-        to: '/order',
-        section: 'order'
+        display: "Orders",
+        icon: <i className="bx bx-receipt"></i>,
+        to: "/order",
+        section: "order",
     },
-]
-
+];
 
 const Refugee = () => {
     const { logout, getAccessTokenSilently, user, isAuthenticated } =
         useAuth0();
 
+    const [helpers, setHelpers] = useState([]);
     console.log(isAuthenticated);
 
-    const callApi = () => {
-        fetch("http://localhost/api/public")
-            .then((res) => console.log(res.message))
-            .catch((err) => console.log(err.message));
-    };
-
-    const callApi2 = async () => {
-        const res = await fetch("http://localhost/api/public");
-        const data = await res.json();
-        console.log(data.message);
-    };
+    // const callApi = () => {
+    //     fetch("http://localhost/api/public")
+    //         .then((res) => console.log(res.message))
+    //         .catch((err) => console.log(err.message));
+    // };
+    //
+    // const callApi2 = async () => {
+    //     const res = await fetch("http://localhost/api/public");
+    //     const data = await res.json();
+    //     console.log(data.message);
+    // };
 
     const getRoute = async () => {
         const token = await getAccessTokenSilently();
@@ -69,38 +69,68 @@ const Refugee = () => {
         const data = await res.json();
         console.log(data);
     };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const token = await getAccessTokenSilently();
+            const res = await fetch("http://localhost/api/helper", {
+                method: "GET",
+                headers: {
+                    "Content-type": "appication/json",
+                    Authorization: "Bearer " + token,
+                },
+            });
+            const data = await res.json();
+            if (data.success) {
+                setHelpers(data.helpers);
+            }
+        };
+
+        if (user) {
+            fetchData();
+        }
+    }, [user]);
+
+    console.log(helpers);
+
     return (
-      <div className="page-container">
-        <div className="container">
-          {isAuthenticated && <div className="profile-details-container" >
-            <div className="image-container">
-              <img src={user.picture}></img>
+        <div className="page-container">
+            <div className="container">
+                {isAuthenticated && (
+                    <div className="profile-details-container">
+                        <div className="image-container">
+                            <img src={user.picture}></img>
+                        </div>
+                        <div className="full-name">
+                            {user.given_name} {user.family_name}
+                        </div>
+
+                        <button
+                            className="main__btn refugee-btn"
+                            onClick={() =>
+                                logout({ returnTo: window.location.origin })
+                            }
+                        >
+                            Log Out
+                        </button>
+                        <button
+                            className="main__btn refugee-btn"
+                            onClick={getRoute}
+                        >
+                            Get Route
+                        </button>
+                    </div>
+                )}
+
+                <div className="content">
+                    {isAuthenticated && (
+                        <pre style={{ textAlign: "start" }}>
+                            {JSON.stringify(user, "salut", 2)}
+                        </pre>
+                    )}
+                </div>
             </div>
-            <div className="full-name">
-              {user.given_name} {user.family_name}
-              </div>
-              
-              <button
-                  className="main__btn refugee-btn"
-                  onClick={() => logout({ returnTo: window.location.origin })}
-              >
-                  Log Out
-              </button>
-              <button className="main__btn refugee-btn" onClick={getRoute}>
-                  Get Route
-              </button>
-          </div> }
-
-          <div className="content">
-
-              {isAuthenticated && (
-                  <pre style={{ textAlign: "start" }}>
-                      {JSON.stringify(user, "salut", 2)}
-                  </pre>
-              )}
-          </div>
         </div>
-      </div>
     );
 };
 
