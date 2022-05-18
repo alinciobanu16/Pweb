7e01553bad9ddfe1a53d26f316f97a0f9f68de00
Alin Ciobanu <alinciobanu1604@gmail.com>
2022-05-18 22:56:06 +0300

    create send-email route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,13 +31,32 @@ Route::get('/public', function () {
 Route::get('/private', function () {
     return response()->json([
         'message' => 'Hello from a private endpoint! You need to be authenticated to see this.',
-        'authorized' => Auth::check(),
-        'user' => Auth::check() ? json_decode(json_encode((array) Auth::user(), JSON_THROW_ON_ERROR), true) : null,
     ], 200, [], JSON_PRETTY_PRINT);
-})->middleware(['auth0.authorize']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/send-email', function () {
+    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+    $channel->queue_declare('task_queue', false, true, false, false);
+    $data = "Hello world";
+    if (empty($data)) {
+        $data = "Hello World!";
+    }
+    $msg = new AMQPMessage(
+        $data,
+        array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+    );
+
+    $channel->basic_publish($msg, '', 'task_queue');
+
+    echo ' [x] Sent ', $data, "\n";
+
+    $channel->close();
+    $connection->close();
 });
 
 Route::group(['middleware' => ['auth0.authorize']], function () {
